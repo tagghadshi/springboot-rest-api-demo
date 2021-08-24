a887459db6e572f851ebe68227de977b36c2b0fe
tagghadshi <tagghadshi@gmail.com>
2021-08-24 07:28:03 +0000

    UserService Added

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -1,5 +1,6 @@
 package com.tcs.springbootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController { // spring
+	
+	@Autowired  //DI
+	IUserService userService;
+	
 	@GetMapping("/getuser")
 	private String getUser() {
 		System.out.println("called");
@@ -15,6 +20,7 @@ public class UserController { // spring
 
 	@PostMapping("/user")
 	private void saveUser(@RequestBody User user) {
+		userService.save(user);
 		System.out.println(user.getFirstName());
 
 	}
